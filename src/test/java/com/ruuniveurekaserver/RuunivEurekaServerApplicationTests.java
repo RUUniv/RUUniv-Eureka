4ca525576f4f1672b55644582f4e9cdd95ba5363
bandalgomsu <q63530@naver.com>
2024-08-13 16:07:26 +0900

    init

---
+++ src/test/java/com/ruuniveurekaserver/RuunivEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruuniveurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuunivEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
